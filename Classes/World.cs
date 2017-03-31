91e8f4a5e9b67c3e70201f818b26d4450d903c25
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-30 23:28:50 -0700

    Update World.cs

--- Classes/World.cs
+++ Classes/World.cs
@@ -1,5 +1,21 @@
-/* The world */
+/* The world - pull everything together to create the game world */
 
 // Multidemensional array to build the size of the world
 
 // build the level, set the size of the array
+  // set the position of the room in the array
+  // create Cave and Item
+  // assign the location
+  // set the title
+  // set the description
+  // add an exit
+  // create an item
+  // set title for item
+  // set description
+  // add item to current room
+
+           
+           
+
+           
+ 
