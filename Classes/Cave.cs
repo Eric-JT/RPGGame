cfb4e9e90c3ecf19b34e2a7e36b6f7a06d33c961
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-30 23:23:15 -0700

    Create Cave.cs

---
+++ Classes/Cave.cs
@@ -0,0 +1,28 @@
+/* Create caves  */
+
+// Store titles for each cave
+
+// Store description of cave
+
+// Store Items in room - list
+
+// store exits in room - list
+
+// Describe()
+        
+// ShowTitle()
+        
+// GetItem()
+       
+// AddExit()
+ 
+// RemoveExit()
+        
+// CanExit()
+       
+// GetItemList()
+       
+// GetExitList()
+       
+// GetCoordinates()
+        
