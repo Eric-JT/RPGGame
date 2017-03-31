d33530faacfdd69b7afcaa29adafa79dc036f88d
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-30 23:14:23 -0700

    Create Enemy.cs

---
+++ Classes/Enemy.cs
@@ -0,0 +1,11 @@
+/* Enemy Class */
+
+// get and set positionX and positionY - integers
+
+// Move methods that increment or decrement the player postionX and positionY
+
+// attack
+
+// get current room
+
+// Health
