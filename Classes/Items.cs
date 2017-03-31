5bb35e68546084a2a497387326ae5f8b87bdd84b
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-30 23:31:53 -0700

    Update Items.cs

--- Classes/Items.cs
+++ Classes/Items.cs
@@ -1,1 +1,12 @@
-/* All items in the game */
+/* building blocks for items */
+
+// store title for item
+
+// store weight of item
+
+// get/set weight
+
+// get set title
+
+// get/set description
+
