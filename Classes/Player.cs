02e7c3ef10ae0a02773bf3a423ad5881680afa11
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-30 23:12:44 -0700

    Create Player.cs

---
+++ Classes/Player.cs
@@ -0,0 +1,19 @@
+/* Player Class */
+
+// get and set positionX and positionY - integers
+
+// List of inventory items
+
+// keep track of maximum weight that a player can carry
+
+// Move methods that increment or decrement the player postionX and positionY
+
+// Pick up items
+
+// drop items
+
+// attack
+
+// get current room
+
+// view inventory
